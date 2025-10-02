0515afaf9e76289a26cbd95f1219e447b4cf98f8
AnujJha88 <anujjha8788@gmail.com>
2025-10-02 15:54:57 +0530

    graph utils planned

--- GraphWidget.h
+++ GraphWidget.h
@@ -8,10 +8,19 @@
 #include "FTXUI/include/ftxui/dom/elements.hpp"
 
 class GraphUtils{
+public:
 
     static ftxui::Element PlotLineGraph(const std::vector<double>& data,const std::string& title="",int width=50,int height=10);
 
     static ftxui::Element PlotBarGraph(const std::vector<std::pair<std::string,double>>& data,const std::string& title="");
 
     static ftxui::Element PlotPriceMovement(const std::vector<double>& prices,const std::string& title="");
+
+    static ftxui::Element PlotOrderSizeDistribution(const std::vector<double>& order_sizes);
+
+private:
+    static std::vector<int> NormalizeData(const std::vector<double>& data,int height);
+    static double FindMin(const std::vector<double>& data);
+    static double FindMax(const std::vector<double>& data);
+
 };
